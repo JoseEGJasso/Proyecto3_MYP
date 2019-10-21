350b956e670da7a8ea39c72c4ee58a66e6e3b97c
Urielglb <uriel_06@ciencias.unam.mx>
2019-10-20 21:06:47 -0500

    clase HTML lista

---
+++ src/main/mx/unam/ciencias/proyecto3/HTML.kt
@@ -0,0 +1,51 @@
+package mx.unam.ciencias.proyecto3
+
+/**
+ * Clase HTML
+ * Esta clase será de la cual hereden todas las clases que deban crear un documento HTML y contendrá los métodos
+ * para agregar los elementos básicos de cualquier documento HTML
+ */
+class HTML {
+
+    protected String HTML = "<!DOCTYPE html>" + "\n" +"<html>"+"\n";//Atributo el se irá modificando según el método para crear el contenido deseado en el documento
+
+    /**
+     * Función agregaNombre que agregará el título en el metadata el documento
+     * @param nombre es un string con el nombre que se desea poner el HTML
+     */
+    protected fun agregaNombre(nombre : String){
+        HTML += "<head>" + "\n" + "<title>" + nombre + "</title>" +"\n" + "</head>" + "\n";
+    }
+
+    /**
+     * Función abreCuerpo que pondrá la etiqueta que abre el segmento de cuerpo en el HTML
+     */
+    protected fun abreCuerpo(){
+        HTML += "<body>" + "\n"
+    }
+
+    /**
+     * Función cierraCuerpo que pondrá la etiqueta que cierre el segmento de cuerpo en el HTML
+     */
+    protected fun cierraCuerpo(){
+        HTML += "\n" + "</body>"
+    }
+
+    /**
+     * Función agregaTitulo que se encargará de poner un titulo siempre de tipo h1 al HTML
+     * @param titulo un string del titulo que se desea agregar
+     */
+    protected fun agregaTitulo(titulo:String){
+        HTML += "<h1>" + titulo + "</h1>"
+    }
+
+    /**
+     * Función que se encargará de poner un parrafo al documento HTML
+     * @param contenido un string con lo que se desea poner en el párrafo
+     */
+    protected fun agregaParrafo(contenido: String) {
+        HTML += "<p>" + contenido + "</p>" + "\n"
+    }
+
+
+}
