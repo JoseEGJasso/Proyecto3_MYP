3d5576dc75f4c4d5125a0b2aaeee03b890324b5a
JoseEGJasso <josejasso2000@ciencias.unam.mx>
2019-10-29 02:01:54 -0600

    empiezo datos

---
+++ src/main/mx/unam/ciencias/proyecto3/Datos.kt
@@ -0,0 +1,9 @@
+package mx.unam.ciencias.proyecto3
+
+/**
+ * Clase que se encarga de conectar la información obtenida de los archivos y las gráficas
+ * que se muestran el el HTML generado
+ */
+class Datos{
+
+}
